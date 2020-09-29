2058ec1a671f1730e516f5625271c3c0e7fdc4d3
rkc25 <61949970+rkc25@users.noreply.github.com>
2020-09-28 21:08:05 -0700

    Add files via upload

--- startgcloud.py
+++ startgcloud.py
@@ -1,6 +1,11 @@
 import os
-os.system("gcloud sql connect myinstance --user=root")
-os.system("gcloud auth login")
-os.system("gcloud config set project augmented-tract-290623")
-os.system("gcloud config set compute/zone us-east4-c")
-os.system("gcloud compute instances start instance-1")
+
+if os.system("gcloud compute instances list"):
+	True
+else:
+	os.system("gcloud sql connect myinstance --user=root")
+	os.system("gcloud auth login")
+	os.system("gcloud config set project augmented-tract-290623")
+	os.system("gcloud config set compute/zone us-east4-c")
+	os.system("gcloud compute instances start instance-1")
+
